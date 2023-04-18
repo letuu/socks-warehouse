84d011c49ae237869c3d9073960779e1f31c5e40
letuu <albertvt@ya.ru>
2023-04-18 17:58:29 +0300

    Initial commit

---
+++ src/test/java/pro/sky/sockswarehouse/SocksWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.sockswarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
